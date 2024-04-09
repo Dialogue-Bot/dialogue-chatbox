368457132dacf62a7dbede6386f1610cf30ec057
Vo Hoang Huy <hoanghuy.dev0210@gmail.com>
2024-04-10 00:04:52 +0700

    fix: typing message

--- src/components/typing-message.tsx
+++ src/components/typing-message.tsx
@@ -3,8 +3,13 @@ import Message from './message'
 export const TypingMessage = () => {
   return (
     <Message
-      isBot={true}
-      message={
+      message={{
+        isBot: true,
+        message: '',
+        template: {} as any,
+        userId: 'typing',
+      }}
+      children={
         <div className='flex space-x-1 h-full flex-1 items-center'>
           <div className='w-1 h-1 bg-neutral-800 rounded-full animate-bounce [animation-delay:-0.3s]'></div>
           <div className='w-1 h-1 bg-neutral-800 rounded-full animate-bounce [animation-delay:-0.15s]'></div>
